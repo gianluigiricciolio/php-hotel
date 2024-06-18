d162f60cb86d2e0fb2fd29c40977cc5a32a8d15a
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-18 21:25:46 +0200

    [UPD] print hotels in table

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+$hotels = [
+
+[
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+],
+[
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+],
+[
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+],
+[
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+],
+[
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Lista di Hotel</h1>
+    <table>
+        <tr>
+            <th>name</th>
+            <th>description</th>
+            <th>parking</th>
+            <th>vote</th>
+            <th>distance_to_center</th>
+        </tr>
+
+        <?php
+        foreach($hotels as $hotel ) {
+            echo "<tr>";
+            foreach($hotel as $key => $value ){
+                echo "<td>{$value}</td>";
+            }
+            echo "</tr>";
+        }
+        
+        ?>
+        
+    </table>
+</body>
+</html>
